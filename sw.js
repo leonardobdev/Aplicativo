4be75cb3569822139c261639ddeb162e850106ef
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-10-19 18:12:56 -0300

    .

--- sw.js
+++ sw.js
@@ -115,7 +115,19 @@ const enableNavigationPreload = async () => {
   }
 };
 
+const deleteCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+  const cacheKeepList = ["v2"];
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  await Promise.all(cachesToDelete.map(deleteCache));
+};
+
 self.addEventListener("activate", (event) => {
+  event.waitUntil(deleteOldCaches());
   event.waitUntil(enableNavigationPreload());
 });
 
