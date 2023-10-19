71083f22ef774da94804e346a65758d7971f3bae
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-10-19 15:27:20 -0300

    .

--- sw.js
+++ sw.js
@@ -1,61 +1,78 @@
-var APP_PREFIX = "SnakeServiceworker_"
-var VERSION = "version_03"
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [
-  "/Snake/img/x1024.png",
-  "/Snake/img/animated_favicon1.gif",
-  "/Snake/favicon.ico",
-  "/Snake/img/logo.svg",
-  "/Snake/sw.js",
-  "/Snake/",
-  "/Snake/index.html",
-  "/Snake/js/materialize.min.js",
-  "/Snake/css/materialize.min.css",
-  "/Snake/material-icons/iconfont/material-icons.css",
-  "/Snake/material-icons/iconfont/material-icons.woff",
-  "/Snake/material-icons/iconfont/material-icons.woff2",
-  "/Snake/manifest.json",
-  "/Snake/app.js"
-];
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open('v1');
+  await cache.addAll(resources);
+};
 
-self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log("responding with cache : " + e.request.url)
-        return request
-      } else {
-        console.log("file is not cached, fetching : " + e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info('using preload response', preloadResponse);
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  try {
+    const responseFromNetwork = await fetch(request.clone());
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
 });
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("installing cache : " + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/Snake/img/x1024.png",
+      "/Snake/img/animated_favicon1.gif",
+      "/Snake/favicon.ico",
+      "/Snake/img/logo.svg",
+      "/Snake/sw.js",
+      "/Snake/",
+      "/Snake/index.html",
+      "/Snake/js/materialize.min.js",
+      "/Snake/css/materialize.min.css",
+      "/Snake/material-icons/iconfont/material-icons.css",
+      "/Snake/material-icons/iconfont/material-icons.woff",
+      "/Snake/material-icons/iconfont/material-icons.woff2",
+      "/Snake/manifest.json",
+      "/Snake/app.js"
+    ])
+  );
 });
 
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      });
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log("deleting cache : " + keyList[i])
-          return caches.delete(keyList[i])
-        }
-      }))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse
     })
-  )
+  );
 });
