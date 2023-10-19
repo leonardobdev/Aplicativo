44a298e31989f6509884649606d1183959458703
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-10-19 16:04:44 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/Snake/sw.js", { scope: "/Snake/" })
-}
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/Snake/sw.js", { scope: "/Snake/" }).then(() => {
+      console.log('Service worker registered!');
+    }).catch((error) => {
+      console.warn('Error registering service worker:');
+      console.warn(error);
+    });
+  }
+});
--- sw.js
+++ sw.js
@@ -1,80 +1,59 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open('v4');
-  await cache.addAll(resources);
-};
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-const putInCache = async (request, response) => {
-  const cache = await caches.open('v4');
-  await cache.put(request, response);
-};
+const PRECACHE_URLS = [
+  "/Snake/img/x1024.png",
+  "/Snake/img/animated_favicon1.gif",
+  "/Snake/favicon.ico",
+  "/Snake/img/logo.svg",
+  "/Snake/sw.js",
+  "/Snake/",
+  "/Snake/index.html",
+  "/Snake/js/materialize.min.js",
+  "/Snake/css/materialize.min.css",
+  "/Snake/material-icons/iconfont/material-icons.css",
+  "/Snake/material-icons/iconfont/material-icons.woff",
+  "/Snake/material-icons/iconfont/material-icons.woff2",
+  "/Snake/manifest.json",
+  "/Snake/app.js"
+];
 
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
-  const preloadResponse = await preloadResponsePromise;
-  if (preloadResponse) {
-    console.info('using preload response', preloadResponse);
-    putInCache(request, preloadResponse.clone());
-    return preloadResponse;
-  }
-
-  try {
-    const responseFromNetwork = await fetch(request.clone());
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-  } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
-    }
-    return new Response('Network error happened', {
-      status: 408,
-      headers: { 'Content-Type': 'text/plain' },
-    });
-  }
-};
-
-const enableNavigationPreload = async () => {
-  event.waitUntil(async function () {
-    if (self.registration.navigationPreload) {
-      await self.registration.navigationPreload.enable();
-    } return;
-  })();
-};
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(enableNavigationPreload());
-});
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    addResourcesToCache([
-      "/Snake/img/x1024.png",
-      "/Snake/img/animated_favicon1.gif",
-      "/Snake/favicon.ico",
-      "/Snake/img/logo.svg",
-      "/Snake/sw.js",
-      "/Snake/",
-      "/Snake/index.html",
-      "/Snake/js/materialize.min.js",
-      "/Snake/css/materialize.min.css",
-      "/Snake/material-icons/iconfont/material-icons.css",
-      "/Snake/material-icons/iconfont/material-icons.woff",
-      "/Snake/material-icons/iconfont/material-icons.woff2",
-      "/Snake/manifest.json",
-      "/Snake/app.js"
-    ])
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    cacheFirst({
-      request: event.request,
-      preloadResponsePromise: event.preloadResponse
-    })
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
   );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
